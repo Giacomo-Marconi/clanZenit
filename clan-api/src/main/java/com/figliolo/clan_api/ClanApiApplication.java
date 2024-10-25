53b8a607899fb328d8eb8978bff4b8c2ba5a831d
Giacomo-Marconi <19894@studenti.marconiverona.edu.it>
2024-10-25 18:29:20 +0200

    template springBoot

---
+++ clan-api/src/main/java/com/figliolo/clan_api/ClanApiApplication.java
@@ -0,0 +1,13 @@
+package com.figliolo.clan_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClanApiApplication.class, args);
+	}
+
+}
---
+++ clan-api/src/test/java/com/figliolo/clan_api/ClanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.figliolo.clan_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
